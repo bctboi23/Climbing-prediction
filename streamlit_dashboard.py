51a18b552a34d3c3e099d14cde55d8e8123e27c0
bctboi23 <35770571+bctboi23@users.noreply.github.com>
2024-05-01 02:22:47 -0500

    New model
    updated model + data cleaning

--- streamlit_dashboard.py
+++ streamlit_dashboard.py
@@ -342,11 +342,11 @@ with col1:
     st.markdown("#### Model metrics")
     col11, col12, col13 = st.columns((1, 1, 1), gap='small')
     with col11:
-        st.metric("Test RMSE", 1.4801)
+        st.metric("Test RMSE", 1.4643)
     with col12:
-        st.metric("Test MAE", 1.2143)
+        st.metric("Test MAE", 1.1986)
     with col13:
-        st.metric("Test R^2", 0.6092)
+        st.metric("Test R^2", 0.6175)
     fig = go.Figure(go.Waterfall(
         name = "20", orientation = "h",
         measure = ["relative" for x in range(len(feature_names))],
